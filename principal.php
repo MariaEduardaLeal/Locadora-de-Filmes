f214f9e7aabf7f7da6beea795dcee5c44b6c9784
Chardaviski <eduardaleal753@gmail.com>
2023-09-18 19:08:43 -0300

    separando funcoes do adm e usuario comum

--- principal.php
+++ principal.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start(); // inicia a sessão
 include('conexao.php');
 
@@ -15,30 +15,32 @@ $id_tipo_usuario = $dado_tipo_usuario['id_tipo_usuario'];
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>principal</title>
 </head>
+
 <body>
     <ul>
-        <?php 
-            if ($id_tipo_usuario == 1) {
-                echo '<li><a href="lista_clientes.php">Lista Clientes</a></li>';
-                echo '<li><a href="edita_cliente.php">Edita Cliente</a></li>';
-                echo '<li><a href="deleta_cliente.php">Deleta Cliente</a></li>';
-                echo '<li><a href="cadastrar_filmes.php">Cadastrar Filmes</a></li>';
-                echo '<li><a href="edita_filme.php">Edita Filme</a></li>';
-                echo '<li><a href="deleta_filme.php">Deleta Filme</a></li>';
-            }
-        ?>
-        
-        <li><a href="lista_filmes.php">Lista Filmes</a></li>
-        
-        <li><a href="cadastrar_locacao.php">Cadastrar Locação</a></li>
-        <li><a href="lista_locacao.php">Lista Locacao</a></li>
-        <li><a href="edita_locacao.php">Edita Locação</a></li>
-        <li><a href="deleta_locacao.php">Deleta Locação</a></li>
+        <?php if ($id_tipo_usuario == 1) : ?> <!--Funções do ADM-->
+            <li><a href="lista_clientes.php">Lista Clientes</a></li>
+            <li><a href="edita_cliente.php">Edita Cliente</a></li>
+            <li><a href="deleta_cliente.php">Deleta Cliente</a></li>
+            <li><a href="cadastrar_filmes.php">Cadastrar Filmes</a></li>
+            <li><a href="edita_filme.php">Edita Filme</a></li>
+            <li><a href="deleta_filme.php">Deleta Filme</a></li>
+            <li><a href="edita_locacao.php">Edita Locação</a></li>
+            <li><a href="deleta_locacao.php">Deleta Locação</a></li>
+
+        <?php elseif ($id_tipo_usuario ==2) : ?> <!-- Funções do usuário -->
+            <li><a href="lista_filmes.php">Lista Filmes</a></li>
+            <li><a href="cadastrar_locacao.php">Cadastrar Locação</a></li>
+            <li><a href="lista_locacao.php">Lista Locacao</a></li>
+        <?php endif; ?>
+
     </ul>
 </body>
+
 </html>
