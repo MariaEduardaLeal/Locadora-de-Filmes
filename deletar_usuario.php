f7fffa0bd594bb1f58c4280718031b16956dbd4b
Chardaviski <eduardaleal753@gmail.com>
2023-09-18 21:13:20 -0300

    deletar usuario funcionando

--- deletar_usuario.php
+++ deletar_usuario.php
@@ -1,3 +1,26 @@
 <?php
 include('conexao.php');
+include('verificacao.php');
+
+$id_usuario = $_POST['idcliente'];
+
+$deletar_usuario = "DELETE FROM login WHERE idcliente = $id_usuario";
+$query_deletar = mysqli_query($conexao, $deletar_usuario);
+
+if (!$query_deletar) {
+    echo "<script>alert('Algo deu errado')</script>";
+    echo "<script>window.location.href='lista_clientes.php'</script>";
+}else {
+    $deletar_cliente = "DELETE FROM cliente WHERE idcliente = $id_usuario";
+    $query_deletar_cliente = mysqli_query($conexao, $deletar_cliente);
+    
+    if ($query_deletar_cliente) {
+        echo "<script>alert('Usuário deletado')</script>";
+        echo "<script>window.location.href='lista_clientes.php'</script>";
+    }else {
+        echo "<script>alert('Algo deu errado')</script>";
+        echo "<script>window.location.href='lista_clientes.php'</script>";
+    }
+}
+
 ?>
--- lista_clientes.php
+++ lista_clientes.php
@@ -10,11 +10,13 @@ $resultado = mysqli_query($conexao, $informacao_cliente);
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Clientes</title>
 </head>
+
 <body>
     <h1>Lista de Clientes</h1>
     <table border="1">
@@ -29,8 +31,8 @@ $resultado = mysqli_query($conexao, $informacao_cliente);
             <th>Deletar Usuário</th>
         </tr>
 
-        <?php if ($resultado->num_rows > 0): ?>
-            <?php while ($row = $resultado->fetch_assoc()): ?>
+        <?php if ($resultado->num_rows > 0) : ?>
+            <?php while ($row = $resultado->fetch_assoc()) : ?>
                 <tr>
                     <td><?= $row["idcliente"] ?></td>
                     <td><?= $row["nomecliente"] ?></td>
@@ -39,14 +41,23 @@ $resultado = mysqli_query($conexao, $informacao_cliente);
                     <td><?= $row["bairro"] ?></td>
                     <td><?= $row["cidade"] ?></td>
                     <td><?= $row["estado"] ?></td>
-                    <td> <a href="deletar_usuario.php">Deletar usuário</a> </td>
+                    <td>
+                        <form action="deletar_usuario.php" method="post">
+                            <input type="hidden" name="idcliente" value="<?= $row["idcliente"] ?>">
+                            <input type="submit" value="Deletar usuário">
+                        </form>
+
+                    </td>
                 </tr>
             <?php endwhile; ?>
-        <?php else: ?>
-            <tr><td colspan='8'>Nenhum cliente encontrado.</td></tr>
+        <?php else : ?>
+            <tr>
+                <td colspan='8'>Nenhum cliente encontrado.</td>
+            </tr>
         <?php endif; ?>
     </table>
 </body>
+
 </html>
 <?php
 // Feche a conexão com o banco de dados
