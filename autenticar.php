8874dd3465ba9ca938adf156b5d5c7bb34ba0c40
Fernando Nonato <105546921+Cyberfn@users.noreply.github.com>
2023-09-21 19:36:12 -0300

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -1,25 +1,35 @@
 <?php
 include('conexao.php');
 
-$login = isset($_POST['login'])? $_POST['login'] : '';
-$senha = isset($_POST['senha'])? $_POST['senha']:'';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-if ($login == '' && $senha == '') {
+if ($login == '' || $senha == '') {
     echo "<script>alert('O campo login e senha são obrigatórios')</script>";
     echo "<script>window.location.href='index.php'</script>";
-}else {
-    $select = "SELECT login, senha FROM login
-    WHERE login = '$login' AND senha = '$senha'";
+} else {
+    // Use prepared statements para evitar injeção SQL
+    $select = "SELECT login, senha FROM login WHERE login = ? AND senha = ?";
+    $stmt = mysqli_prepare($conexao, $select);
+    
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "ss", $login, $senha);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $dbLogin, $dbSenha);
+        mysqli_stmt_fetch($stmt);
 
-    $query_selecionar = mysqli_query($conexao, $select);
-    $dado = mysqli_fetch_row($query_selecionar);
+        if ($login == $dbLogin && $senha == $dbSenha) {
+            session_start();
+            $_SESSION['login'] = $dbLogin;
+            header("location: principal.php");
+        } else {
+            echo "<script>alert('Login ou senha inválidos')</script>";
+            echo "<script>window.location.href='index.php'</script>";
+        }
 
-    if($login == isset($dado[0]) && $senha == isset($dado[1])){
-        session_start();
-        $_SESSION['login'] = $dado[0];
-        header("location: principal.php");
-    }else{
-        echo "<script>alert('Login ou senha inválidos')</script>";
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "<script>alert('Erro na consulta SQL')</script>";
         echo "<script>window.location.href='index.php'</script>";
     }
 }
